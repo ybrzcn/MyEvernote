e3b5ecd555703dee23781328731c8e29160b1a09
Aybar Özcan <ybrozcan@gmail.com>
2024-05-11 00:49:55 +0300

    first commit

---
+++ MyEvernote.Entities/EvernoteUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.Entities
+{
+    public class EvernoteUser : MyEntitieBase
+    {
+        [StringLength(25)]
+        public string Name { get; set; }
+        [StringLength(25)]
+        public string Surname { get; set; }
+        [Required,StringLength(25)]
+        public string Username { get; set; }
+        [Required, StringLength(70)]
+        public string Email { get; set; }
+        [Required, StringLength(25)]
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        [Required]
+        public Guid ActivateGuid { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+
+        public virtual List<Note> Notes { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Liked> Likes { get; set; }
+
+    }
+}
