94757af9e393fa5b65f95a4923781189eb4ed904
ybrzcn <ybrozcan@gmail.com>
2024-07-08 23:11:28 +0300

    ilerleme1

--- MyEvernote.BusinessLayer/Test.cs
+++ MyEvernote.BusinessLayer/Test.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyEvernote.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,70 @@ namespace MyEvernote.BusinessLayer
 {
     public class Test
     {
+        private Repository<EvernoteUser> repo_user = new Repository<EvernoteUser>();
+        private Repository<Category> repo_category = new Repository<Category>();
+        private Repository<Comment> repo_comment = new Repository<Comment>();
+        private Repository<Note> repo_note = new Repository<Note>();
+
         public Test()
         {
-            DataAccessLayer.DatabaseContext db = new DataAccessLayer.DatabaseContext();
-            db.Database.CreateIfNotExists();
+            List<Category> categories = repo_category.List();
+            //List<Category> categories_filtered = repo_category.List(x => x.Id > 5);
+        }
+
+        public void InsertTest()
+        {
+            int result = repo_user.Insert(new EvernoteUser()
+            {
+                Name = "aaa",
+                Surname = "bbb",
+                Email = "ybrozcan@gmail.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = true,
+                Username = "aabb",
+                Password = "111",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "aabb"
+            });
+        }
+        public void UpdateTest()
+        {
+            EvernoteUser user = repo_user.Find(x => x.Username == "aabb");
+
+            if (user != null)
+            {
+                user.Username = "xxx";
+
+                int result = repo_user.Update(user);
+            }
+        }
+        public void DeleteTest()
+        {
+            EvernoteUser user = repo_user.Find(x => x.Username == "xxx");
+
+            if (user != null)
+            {
+                int result = repo_user.Delete(user);
+            }
+        }
+        public void CommentTest()
+        {
+            EvernoteUser user = repo_user.Find(x => x.Id == 1);
+            Note note = repo_note.Find(x => x.Id == 3);
+
+            Comment comment = new Comment()
+            {
+                Text = "Bu bir testtir.",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now,
+                ModifiedUsername = "aybarozcan",
+                Note = note,
+                Owner = user
+            };
+
+            repo_comment.Insert(comment);
         }
     }
 }
---
+++ MyEvernote.DataAccessLayer/MyInitialzier.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MyEvernote.Entities;
+
+namespace MyEvernote.DataAccessLayer
+{
+    public class MyInitialzier : CreateDatabaseIfNotExists<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            // adding admin user
+            EvernoteUser admin = new EvernoteUser()
+            {
+                Name = "Aybar",
+                Surname = "Özcan",
+                Email = "ybrozcan@gmail.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = true,
+                Username = "aybarozcan",
+                Password = "123456",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "aybarozcan"
+            };
+
+            // adding standart user
+            EvernoteUser standartUser = new EvernoteUser()
+            {
+                Name = "Bora",
+                Surname = "Özcan",
+                Email = "ybrozcan@gmail.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = false,
+                Username = "boraozcan",
+                Password = "123456",
+                CreatedOn = DateTime.Now.AddHours(1),
+                ModifiedOn = DateTime.Now.AddMinutes(65),
+                ModifiedUsername = "aybarozcan"
+            };
+
+            context.EvernoteUsers.Add(admin);
+            context.EvernoteUsers.Add(standartUser);
+
+            for (int i = 0; i < 8; i++)
+            {
+                EvernoteUser user = new EvernoteUser()
+                {
+                    Name = FakeData.NameData.GetFirstName(),
+                    Surname = FakeData.NameData.GetSurname(),
+                    Email = FakeData.NetworkData.GetEmail(),
+                    ActivateGuid = Guid.NewGuid(),
+                    IsActive = true,
+                    IsAdmin = false,
+                    Username = $"user{i}",
+                    Password = "123",
+                    CreatedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                    ModifiedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                    ModifiedUsername = $"user{i}"
+                };
+
+                context.EvernoteUsers.Add(user);
+            }
+
+            context.SaveChanges();
+
+            // userr list for using..
+            List<EvernoteUser> userList = context.EvernoteUsers.ToList();
+
+            // adding fake categories
+            for (int i = 0; i < 10; i++)
+            {
+                Category cat = new Category()
+                {
+                    Title = FakeData.PlaceData.GetStreetName(),
+                    Description = FakeData.PlaceData.GetAddress(),
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = DateTime.Now.AddMinutes(5),
+                    ModifiedUsername = "aybarozcan"
+                };
+
+                context.Categories.Add(cat);
+
+                // adding fake notes
+                for (int k = 0; k < FakeData.NumberData.GetNumber(5,9); k++)
+                {
+                    EvernoteUser owner = userList[FakeData.NumberData.GetNumber(0, userList.Count - 1)];
+
+                    Note note = new Note()
+                    {
+                        Title = FakeData.TextData.GetAlphabetical(FakeData.NumberData.GetNumber(5, 25)),
+                        Text = FakeData.TextData.GetSentences(FakeData.NumberData.GetNumber(1, 3)),
+                        Category = cat,
+                        IsDraft = false,
+                        LikeCount = FakeData.NumberData.GetNumber(1,9),
+                        Owner = owner,
+                        CreatedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1),DateTime.Now),
+                        ModifiedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1),DateTime.Now),
+                        ModifiedUsername = owner.Username,
+                    };
+
+                    cat.Notes.Add(note);
+
+                    // adding fake comments
+                    for (int j = 0; j < FakeData.NumberData.GetNumber(3,5); j++)
+                    {
+                        EvernoteUser comment_owner = userList[FakeData.NumberData.GetNumber(0, userList.Count - 1)];
+
+                        Comment comment = new Comment()
+                        {
+                            Text = FakeData.TextData.GetSentence(),
+                            Owner = comment_owner,
+                            CreatedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                            ModifiedOn = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-1), DateTime.Now),
+                            ModifiedUsername = comment_owner.Username,
+                        };
+
+                        note.Comments.Add(comment);
+                    }
+
+                    // adding fake likes..
+
+                    
+                    for (int m = 0; m < note.LikeCount; m++)
+                    {
+                        Liked liked = new Liked()
+                        {
+                            LikedUser = userList[m]
+                        };
+
+                        note.Likes.Add(liked);
+                    }
+                }
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
